b0f502deda1f2d150775705869379c26459dc145
bsester <bensester@outlook.com>
2023-12-05 14:46:14 -0600

    Added some cards and testing function

---
+++ js/game.js
@@ -0,0 +1,14 @@
+let cards =
+    [
+        {name: "2_of_clubs", value: 2, img: "imgs/cards/2_of_clubs.png"},
+        {name: "2_of_diamonds", value: 2, img: "imgs/cards/2_of_diamonds.png"},
+        {name: "2_of_hearts", value: 2, img: "imgs/cards/2_of_hearts.png"},
+        {name: "2_of_spades", value: 2, img: "imgs/cards/2_of_clubs.png"}
+    ]
+function cardTest()
+{
+    for (let i=0; i<cards.length;i++)
+    {
+        console.log(cards[i]);
+    }
+}
