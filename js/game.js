db191821ae867e76308e3c2a77acb9927c8f6158
bsester <bensester@outlook.com>
2023-12-05 14:48:47 -0600

    debug code

--- js/game.js
+++ js/game.js
@@ -7,8 +7,12 @@ let cards =
     ]
 function cardTest()
 {
+    let oStr = "";
     for (let i=0; i<cards.length;i++)
     {
         console.log(cards[i]);
+        oStr += `<img src ="${cards[i].img}"`;
     }
+    let results = document.getElementById("play");
+    results.innerHTML = oStr;
 }
