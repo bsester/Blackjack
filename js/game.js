453f71e58948374ea05a76703a198c9a5909ccb3
bsester <124089310+bsester@users.noreply.github.com>
2023-12-05 17:39:09 -0600

    cards work

--- js/game.js
+++ js/game.js
@@ -1,37 +1,4 @@
-let cards =
-    [
-        {name: "2_of_clubs", value: 2, img: "imgs/cards/2_of_clubs.png"},
-        {name: "2_of_diamonds", value: 2, img: "imgs/cards/2_of_diamonds.png"},
-        {name: "2_of_hearts", value: 2, img: "imgs/cards/2_of_hearts.png"},
-        {name: "2_of_spades", value: 2, img: "imgs/cards/2_of_clubs.png"},
-        {name: "3_of_clubs", value: 3, img: "imgs/cards/3_of_clubs.png"},
-        {name: "3_of_diamonds", value: 3, img: "imgs/cards/3_of_diamonds.png"},
-        {name: "3_of_hearts", value: 3, img: "imgs/cards/3_of_hearts.png"},
-        {name: "3_of_spades", value: 3, img: "imgs/cards/3_of_clubs.png"},
-        {name: "4_of_clubs", value: 4, img: "imgs/cards/4_of_clubs.png"},
-        {name: "4_of_diamonds", value: 4, img: "imgs/cards/4_of_diamonds.png"},
-        {name: "4_of_hearts", value: 4, img: "imgs/cards/4_of_hearts.png"},
-        {name: "4_of_spades", value: 4, img: "imgs/cards/4_of_clubs.png"},
-        {name: "5_of_clubs", value: 6, img: "imgs/cards/5_of_clubs.png"},
-        {name: "5_of_diamonds", value: 5, img: "imgs/cards/5_of_diamonds.png"},
-        {name: "5_of_hearts", value: 5, img: "imgs/cards/5_of_hearts.png"},
-        {name: "5_of_spades", value: 5, img: "imgs/cards/5_of_clubs.png"},
-        {name: "6_of_clubs", value: 6, img: "imgs/cards/6_of_clubs.png"},
-        {name: "6_of_diamonds", value: 6, img: "imgs/cards/6_of_diamonds.png"},
-        {name: "6_of_hearts", value: 6, img: "imgs/cards/6_of_hearts.png"},
-        {name: "6_of_spades", value: 6, img: "imgs/cards/6_of_clubs.png"},
-        {name: "7_of_clubs", value: 7, img: "imgs/cards/7_of_clubs.png"},
-        {name: "7_of_diamonds", value: 7, img: "imgs/cards/7_of_diamonds.png"},
-        {name: "7_of_hearts", value: 7, img: "imgs/cards/7_of_hearts.png"},
-        {name: "7_of_spades", value: 7, img: "imgs/cards/7_of_clubs.png"},
-        {name: "8_of_clubs", value: 8, img: "imgs/cards/8_of_clubs.png"},
-        {name: "8_of_diamonds", value: 8, img: "imgs/cards/8_of_diamonds.png"},
-        {name: "8_of_hearts", value: 8, img: "imgs/cards/8_of_hearts.png"},
-        {name: "8_of_spades", value: 8, img: "imgs/cards/8_of_clubs.png"},
-
-
-
-    ]
+const cards = require('cards.js');
 function cardTest()
 {
     let oStr = "";
